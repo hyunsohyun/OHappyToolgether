ee7f31cda2a035a023cb371090ac81ccfb4c471f
hyunsohyun <llikepsh515@gmail.com>
2023-06-28 15:40:54 +0900

    feat: 게시글 백 세팅 + 리스트페이지

---
+++ src/main/java/kr/or/kosa/post/PostController.java
@@ -0,0 +1,33 @@
+package kr.or.kosa.post;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PostController {
+
+	private static final Logger logger = LoggerFactory.getLogger(PostController.class);
+
+	private PostService postService;
+
+	@Autowired
+	public void setPostService(PostService postService) {
+		this.postService = postService;
+	}
+
+	/* 게시판 템플릿 페이지*/
+	@RequestMapping("/postList.do")
+	public String postList(Model model) throws Exception{
+		List<Post> postlist = postService.postlist();
+		model.addAttribute("list", postlist);
+
+		return "post/postList";
+	}
+
+}
