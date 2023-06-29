da411efb07245049a3fc694d233acb230cf36877
osh <osh8242@gmail.com>
2023-06-29 15:56:32 +0900

    feat. Project & UserProject CRUD 완성

---
+++ src/main/java/kr/or/kosa/member/controller/MemberController.java
@@ -0,0 +1,107 @@
+package kr.or.kosa.member.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.or.kosa.member.service.MemberService;
+import kr.or.kosa.member.vo.Users;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+	
+	MemberService memberService;
+	
+	@Autowired
+	public void setMemberService(MemberService memberService) {
+		this.memberService = memberService;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Users>> selectAllUser() {
+		List<Users> list = null;
+		try {
+			list = memberService.selectAllUser();
+			return new ResponseEntity<List<Users>>(list,HttpStatus.OK);
+		} catch (Exception e) {			
+			System.out.println("emplist()에서 터짐");
+			System.out.println(e.getMessage());
+			return new ResponseEntity<List<Users>>(list,HttpStatus.BAD_REQUEST);
+		}		
+	}
+	
+	@GetMapping("{userid}")
+	public ResponseEntity<Users> selectUserById(@PathVariable("userid") String userid) {
+		Users user = null;
+		try {
+			user = memberService.selectUserById(userid);
+			System.out.println("컨트롤러에서 받은 user 정보 : " + user.toString());
+			
+			return new ResponseEntity<Users>(user,HttpStatus.OK);
+		} catch (Exception e) {			
+			System.out.println("selectUserById()에서 터짐");
+			System.out.println(e.getMessage());
+			return new ResponseEntity<Users>(user,HttpStatus.BAD_REQUEST);
+		}		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Integer> insertUser(@RequestBody Users user) {
+		int result = 0;
+		try {
+			result = memberService.insertUser(user);
+			System.out.println("requestBody의 user 정보 : " + user.toString());
+			
+			return new ResponseEntity<Integer>(result,HttpStatus.OK);
+		} catch (Exception e) {			
+			System.out.println("insertUser()에서 터짐");
+			System.out.println(e.getMessage());
+			return new ResponseEntity<Integer>(result,HttpStatus.BAD_REQUEST);
+		}		
+	}
+	
+	@DeleteMapping({"userid"})
+	public ResponseEntity<Integer> deleteUser(@PathVariable("userid") String userid) {
+		int result = 0;
+		try {
+			result = memberService.deleteUser(userid);
+			System.out.println("삭제 요청에 따라 다음 행의 개수가 삭제됨 : "+result);			
+			return new ResponseEntity<Integer>(result,HttpStatus.OK);
+		} catch (Exception e) {			
+			System.out.println("deleteUser()에서 터짐");
+			System.out.println(e.getMessage());
+			return new ResponseEntity<Integer>(result,HttpStatus.BAD_REQUEST);
+		}		
+	}
+	
+	@PutMapping
+	public ResponseEntity<Integer> updateUser(@RequestBody Users user) {
+		int result = 0;
+		try {
+			result = memberService.updateUser(user);
+			System.out.println("requestBody의 user 정보 : " + user.toString());
+			
+			return new ResponseEntity<Integer>(result,HttpStatus.OK);
+		} catch (Exception e) {			
+			System.out.println("insertUser()에서 터짐");
+			System.out.println(e.getMessage());
+			return new ResponseEntity<Integer>(result,HttpStatus.BAD_REQUEST);
+		}		
+	}
+	
+	
+	
+}
---
+++ src/main/java/kr/or/kosa/project/service/ProjectService.java
@@ -0,0 +1,94 @@
+package kr.or.kosa.project.service;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.or.kosa.project.dao.ProjectDao;
+import kr.or.kosa.project.vo.Project;
+import kr.or.kosa.project.vo.UsersProject;
+
+@Service
+public class ProjectService {
+	
+	SqlSession sqlsession;
+	
+	@Autowired
+	public void setSqlsession(SqlSession sqlsession) {
+		this.sqlsession = sqlsession;
+	}
+	
+	public List<Project> selectAllProjectById(String userid){
+		System.out.println("selectAllProjectById() 시작");
+		List<Project> list = null;
+		ProjectDao dao = sqlsession.getMapper(ProjectDao.class);
+		System.out.println("ProjectDao 맵퍼 가져옴");
+		try {
+			list = dao.selectAllProjectById(userid);
+			System.out.println("list 사이즈 : " + list.size());
+			System.out.println("projectList 가져옴");
+		} catch (Exception e) {
+			System.out.println("dao.selectAllProjectById(userid) 에서 터짐");
+			e.getStackTrace();
+			System.out.println(e.getMessage());
+		}		
+		System.out.println("selectAllProjectById 리턴");
+		return list;
+	}
+	
+	public int insertProject(Project project) {
+		int result = 0;
+		ProjectDao dao = sqlsession.getMapper(ProjectDao.class);
+		System.out.println("ProjectDao 맵퍼 가져옴");
+		try {
+			result = dao.insertProject(project);
+			System.out.println("insertProject 처리함");
+		} catch (Exception e) {
+			e.getStackTrace();
+		}		
+		return result;
+	}
+	
+	public int deleteProject(String projectId) {
+		int result = 0;
+		ProjectDao dao = sqlsession.getMapper(ProjectDao.class);
+		System.out.println("ProjectDao 맵퍼 가져옴");
+		try {
+			result = dao.deleteProject(projectId);
+			System.out.println("deleteProject 처리함");
+		} catch (Exception e) {
+			e.getStackTrace();
+		}		
+		return result;
+	}
+	
+	public int insertUsersProject(UsersProject usersProject) {
+		int result = 0;
+		ProjectDao dao = sqlsession.getMapper(ProjectDao.class);
+		System.out.println("ProjectDao 맵퍼 가져옴");		
+		System.out.println("userid : " + usersProject.getUserid());
+		System.out.println("projectId : " + usersProject.getProjectId());
+		try {			
+			result = dao.insertUsersProject(usersProject);
+			System.out.println("insertUsersProject 처리함");
+		} catch (Exception e) {
+			e.getStackTrace();
+		}		
+		return result;
+	}
+	
+	public int deleteUsersProject(UsersProject usersProject) {
+		int result = 0;
+		ProjectDao dao = sqlsession.getMapper(ProjectDao.class);
+		System.out.println("ProjectDao 맵퍼 가져옴");			
+		try {			
+			result = dao.deleteUsersProject(usersProject);
+			System.out.println("deleteUsersProject 처리함");
+		} catch (Exception e) {
+			e.getStackTrace();
+		}		
+		return result;
+	}
+}
